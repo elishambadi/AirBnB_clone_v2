c27c15a9f72b5c50d14618e16963c14865d35f08
elishambadi <embadi43@gmail.com>
2022-11-10 12:06:52 +0300

    fix pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,11 @@ from flask import Flask
 
 app = Flask('__main__')
 
+
 @app.route('/', strict_slashes=False)
 def index():
-    return 'Hello HBNB!\n'
+    return 'Hello HBNB!'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000', debug=True)
