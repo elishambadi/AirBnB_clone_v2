00472d318ff18189854891ab07b04a2516e2e4c8
elishambadi <embadi43@gmail.com>
2022-11-10 12:27:18 +0300

    add task 2 - params

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+   Starts a flask web app
+"""
+from flask import Flask, escape
+app = Flask('__main__')
+
+
+@app.route('/', strict_slashes=False)
+def index_route():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    return 'C {}'.format(escape(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
