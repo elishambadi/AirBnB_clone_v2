dafaba50301bdc5fcf71e0e68855f5b8afc3c23d
elishambadi <embadi43@gmail.com>
2022-11-10 14:28:24 +0300

    add task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+   Starts a flask web app
+"""
+from flask import Flask, escape, abort, render_template
+app = Flask('__main__')
+
+
+@app.route('/', strict_slashes=False)
+def index_route():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    return 'C {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route('/python')
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    return 'Python {}'.format(escape(text.replace("_", " ")))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number_route(n):
+    try:
+        n = int(n)
+        return '{} is a number'.format(escape(n))
+    except ValueError:
+        abort(404)
+
+
+@app.route('/number_template/<n>', strict_slashes=False)
+def number_temp_route(n):
+    try:
+        n = int(n)
+        return render_template('5-number.html', number=n)
+    except ValueError:
+        abort(404)
+
+
+@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+def number_odd_even_route(n):
+    try:
+        n = int(n)
+        return render_template('6-number_odd_or_even.html', number=n)
+    except ValueError:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
